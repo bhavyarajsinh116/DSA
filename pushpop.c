0d46ff2f3c91f088ad456fa0576f88e2428d480e
Bhavyarajsinh Gohil <166473838+bhavyarajsinh116@users.noreply.github.com>
2024-09-17 10:29:09 +0530

    Add files via upload
    push pop function in c language

---
+++ pushpop.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void push(struct stack *ptr, int value)
+{
+    if (isFull(ptr))
+    {
+        printf("stack is overflow!!\n");
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("stack is underflow!!\n");
+        return -1;
+    }
+    else
+    {
+        int value = ptr->arr[ptr->top];
+        ptr->top--;
+        return value;
+    }
+}
+int main()
+{
+    int x, n, y, choise;
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    while (1)
+    {
+       
+
+        printf("---------------------------------\n");
+        printf("1.push the value in stack\n");
+        printf("2.pop the in stack\n");
+        printf("3.print the value\n");
+        printf("4.exit\n");
+        printf("---------------------------------\n");
+        printf("what is your choise : ");
+        scanf("%d", &choise);
+        switch (choise)
+        {
+        case 1:
+            printf("how many values you want enter : ");
+            scanf("%d", &n);
+            for (int i = 0; i < n; i++)
+            {
+                scanf("%d", &x);
+                push(sp, x);
+            }
+            break;
+
+        case 2:
+            printf("how many you want to delete in stack\n");
+            scanf("%d", &y);
+            for (int i = 0; i < y; i++)
+            {
+                pop(sp);
+            }
+            break;
+        case 3:
+            printf("your value are : ");
+            for (int i = 0; i <=sp->top; i++)
+            {
+
+                printf("%d\t", sp->arr[i]);
+            }
+            printf("\n");
+            break;
+        case 4:
+            exit(0);
+           break;
+
+        default:
+        printf("error!!\n");
+        }
+
+        
+    } 
+
+    
+
+    return 0;
+}
